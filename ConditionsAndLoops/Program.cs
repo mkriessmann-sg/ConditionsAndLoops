e772d833d96cae095122b24b304735f989b161e0
MARCO KRIESSMANN <kriessmann-marco@s-giken.com>
2024-04-04 13:50:00 +0900

    fixed clarity with elseif

--- ConditionsAndLoops/Program.cs
+++ ConditionsAndLoops/Program.cs
@@ -15,27 +15,21 @@
                 {
                     Console.WriteLine("FizzBuzz");
                 }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
                 else
                 {
-                    if (i%3 ==0)
-                    {
-                        Console.WriteLine("Fizz");
-                    }
-                    else
-                    {
-                            if(i%5 ==0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(i);
-                        }
-                    }
+                    Console.WriteLine(i);
                 }
             }
-
-
         }
     }
 }
+ 
+
